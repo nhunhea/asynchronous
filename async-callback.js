f3b3b440233aedf51f891c5ced361d35e8c97d95
Evania <evan.nhea@gmail.com>
2018-02-26 14:21:41 +0700

    revision 2

--- async-callback.js
+++ async-callback.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
-var path = require('path');
+var path = cdrequire('path');
 
 console.log('Tes 1');
-fs.readFile(path.join(path.dirname(__filename), 'tes.txt'), 'utf8', function(error, contents){
+fs.readFile(path.join(path.dirname(__filename), 'tes.txt'), 'utf8', function(error, contents) {
   console.log(contents);
 });
-fs.readFile('./tes.txt', 'utf8', function(error, contents){
+fs.readFile('./tes.txt', 'utf8', function(error, contents) {
   console.log(contents);
 });
 console.log('Tes 3');
